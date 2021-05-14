a83e2546d955b5b16b0b110c48b7d408b8168859
carcoindevs <carcoindevs@gmail.com>
2021-05-14 12:28:23 +0200

    changed title

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -1,7 +1,7 @@
 import { PageMeta } from './types'
 
 export const DEFAULT_META: PageMeta = {
-  title: 'LibreSwap',
+  title: 'CarPool Finance',
   description:
     'The least popular AMM on BSC by user count! Earn LIBRE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by LibreSwap), NFTs, and more, on a platform you can trust.',
   image: 'https://pancakeswap.finance/images/easter-battle.png',
@@ -9,7 +9,7 @@ export const DEFAULT_META: PageMeta = {
 
 export const customMeta: { [key: string]: PageMeta } = {
   '/competition': {
-    title: 'LibreSwap',
+    title: 'CarPool Finance',
     description: 'Register now for the PancakeSwap Easter battle!',
     image: 'https://pancakeswap.finance/images/easter-battle.png',
   },
