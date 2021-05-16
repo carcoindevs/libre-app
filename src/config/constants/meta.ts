0290c83134cf84bfa0b685e19d2e3755bf058356
carcoindevs <carcoindevs@gmail.com>
2021-05-16 12:10:53 +0200

    changed CAKE and LIBRE to CPL in home

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -3,7 +3,7 @@ import { PageMeta } from './types'
 export const DEFAULT_META: PageMeta = {
   title: 'CarPool Finance',
   description:
-    'The least popular AMM on BSC by user count! Earn LIBRE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by LibreSwap), NFTs, and more, on a platform you can trust.',
+    'The least popular AMM on BSC by user count! Earn CPL through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by LibreSwap), NFTs, and more, on a platform you can trust.',
   image: 'https://pancakeswap.finance/images/easter-battle.png',
 }
 
