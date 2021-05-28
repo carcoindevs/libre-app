df196c9131e1aec9e524e657739f4f160daaf54e
carcoindevs <carcoindevs@gmail.com>
2021-05-28 23:27:22 +0200

    ambitious change to pageloader

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Spinner } from '@tiscotservices/carpooluikit'
 import Page from './layout/Page'
 
 const Wrapper = styled(Page)`
@@ -9,10 +8,31 @@ const Wrapper = styled(Page)`
   align-items: center;
 `
 
+const Hero = styled.div`
+  align-items: center;
+  background-image: url('/images/pan-bg-mobile.svg');
+  background-repeat: no-repeat;
+  background-position: top center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: auto;
+  margin-bottom: 32px;
+  padding-top: 116px;
+  text-align: center;
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    background-image: url('/images/pan-bg2.png');
+    background-position: center;
+    height: 165px;
+    padding-top: 0;
+  }
+`
+
 const PageLoader: React.FC = () => {
   return (
     <Wrapper>
-      <Spinner />
+      <Hero />
     </Wrapper>
   )
 }
